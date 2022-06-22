53acb1f23f028803b9bfad822c7e941eae512cf7
Ethernal-Serenade <nguyenduclam0605@gmail.com>
2022-06-22 16:04:19 +0700

    setup: conn_db

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
+import os
 from flask import Flask, make_response
-# from pymongo import MongoClient
+from pymongo import MongoClient
+import json
+from bson import json_util
+
 
 app = Flask(__name__)
 
-
-# def connect():
-#     db_user = "mongo"
-#     db_pass = "0815985051"
-#     db_addr = "localhost:27017"
-#     uri = "mongodb://{0}:{1}@{2}".format(db_user, db_pass, db_addr)
-#     client = MongoClient(uri, serverSelectionTimeoutMS=6000)
-#     return client
+def connect():
+    uri = os.environ.get("DATABASE_URL")
+    client = MongoClient(uri, serverSelectionTimeoutMS=6000)
+    return client
 
 
 @app.before_first_request
 def init_app():
     # TODO: init mongo db here
-    # connect()
+
     # TODO: init grpc server here
-    print("Hello Eatiplan")
+    print("test")
 
 
 @app.route("/health")
 def health():
     return make_response({}, 200)
+
+
+
+@app.route("/conn_db")
+def conn_db():
+    client = connect()
+    db = client["eatiplan-user-service"]
+    User = db.User
+    res = json.loads(json_util.dumps(User.find_one()))
+    return res
