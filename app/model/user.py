747108bb06c333e4b08fea2200a3b2e4a9196bdf
Ethernal-Serenade <nguyenduclam0605@gmail.com>
2022-06-29 01:55:46 +0700

    feat: add validation for user model

--- app/model/user.py
+++ app/model/user.py
@@ -1,3 +1,5 @@
+from enum import unique
+
 from mongoengine import BooleanField, \
     Document, ListField, ReferenceField, StringField
 
@@ -6,10 +8,14 @@ from .address import Address
 
 class User(Document):
     username = StringField(max_length=20, required=True, unique=True)
-    password = StringField(required=True)
+    password = StringField(min_length=8, required=True,
+                           regex='^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)'
+                                 '(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$')
     first_name = StringField(max_length=10, required=True)
     last_name = StringField(max_length=10, required=True)
-    email = StringField(required=True)
+    email = StringField(required=True, unique=True,
+                        regex='[a-z0-9._%+!$&*=^|~#%{}/-]+@'
+                              '([a-z0-9-]+.){1,}([a-z]{2,22})')
     phone = StringField()
     birthday = StringField()
     is_kol = BooleanField()
