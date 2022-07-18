dd59a497acdfa60d0a942e9021353828fcb45763
Louis <huynguyennbk@gmail.com>
2022-07-18 13:23:07 +0700

    refactor: update profile using token

--- app/grpc/server/services/concerns/process.py
+++
@@ -1,8 +0,0 @@
-import jwt
-import os
-
-
-def get_userid_from_token(token):
-    access_token = os.getenv("JWT_ACCESS_SECRET")
-    payload = jwt.decode(token, key=access_token, algorithm=["HS256"])
-    return payload.user_id
--- app/grpc/server/services/profile.py
+++ app/grpc/server/services/profile.py
@@ -1,10 +1,10 @@
+from google.protobuf.json_format import MessageToDict
 import grpc
 from mongoengine import DoesNotExist
 
-from google.protobuf.json_format import MessageToDict
-from app.grpc.client.decorators.auth import authenticated, _authorization_key
+from app.grpc.client.decorators.auth import authenticated
+
 from app.grpc.generated import profile_pb2_grpc, profile_pb2
-from .concerns.process import get_userid_from_token
 from app.services.profile import update_profile
 
 
@@ -12,8 +12,10 @@ class ProfileService(profile_pb2_grpc.ProfileService):
     @authenticated
     def UpdateProfile(self, request, context):
         try:
-            user_id = get_userid_from_token(_authorization_key(context))
-            user = update_profile(user_id=user_id, data=MessageToDict(request.data))
+            user = update_profile(
+                user_id=context.user_id,
+                data=MessageToDict(request.data)
+            )
 
             return profile_pb2.UpdateProfileResponse(**user.proto_data())
         except DoesNotExist as e:
