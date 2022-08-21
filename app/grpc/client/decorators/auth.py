28d9a2437cb27ba15b8b770c0561f28d3bb9bbeb
Louis <huynguyennbk@gmail.com>
2022-08-22 02:05:03 +0700

    feat: check user active in user service instead of calling ValidActionToken rpc

--- app/grpc/client/decorators/auth.py
+++ app/grpc/client/decorators/auth.py
@@ -4,6 +4,7 @@ import grpc
 from app.grpc.client import grpc_channel
 from app.grpc.generated import jwt_pb2_grpc
 from app.grpc.utils.jwt import authorization_key, get_user_id_from_token
+from app.model.user import User
 
 
 def activated(func):
@@ -12,17 +13,22 @@ def activated(func):
             stub = jwt_pb2_grpc.JwtServiceStub(channel)
 
             try:
-                response = stub.ValidActivationToken(empty_pb2.Empty(), metadata=(
+                response = stub.ValidToken(empty_pb2.Empty(), metadata=(
                     ('authorization', authorization_key(context)),
                 ))
 
-                if response.valid is True:
-                    user_id = get_user_id_from_token(authorization_key(context))
-                    context.user_id = user_id
-
-                    return func(self, request, context)
-                else:
+                if response.valid is not True:
+                    context.set_details(repr('Token is invalid'))
                     raise grpc.RpcError()
+
+                user_id = get_user_id_from_token(authorization_key(context))
+                if not User.objects.get(ID=user_id).is_active:
+                    context.set_details(repr('User has not been activated yet'))
+                    raise grpc.RpcError()
+
+                context.user_id = user_id
+
+                return func(self, request, context)
             except grpc.RpcError as rpc_error:
                 if rpc_error is not None:
                     context.set_details(repr(rpc_error.details()))
@@ -41,13 +47,14 @@ def authenticated(func):
                     ('authorization', authorization_key(context)),
                 ))
 
-                if response.valid is True:
-                    user_id = get_user_id_from_token(authorization_key(context))
-                    context.user_id = user_id
-
-                    return func(self, request, context)
-                else:
+                if response.valid is not True:
+                    context.set_details(repr('Token is invalid'))
                     raise grpc.RpcError()
+
+                user_id = get_user_id_from_token(authorization_key(context))
+                context.user_id = user_id
+
+                return func(self, request, context)
             except grpc.RpcError as rpc_error:
                 if rpc_error is not None:
                     context.set_details(repr(rpc_error.details()))
